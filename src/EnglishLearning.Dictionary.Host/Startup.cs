4b9c45f59ae2c9e29b7f4e40435f9cf087904871
LytvyniukDima <dima.lytvyniuk@gmail.com>
2021-03-11 07:45:47 +0200

    feat: add english learning identity

--- src/EnglishLearning.Dictionary.Host/Startup.cs
+++ src/EnglishLearning.Dictionary.Host/Startup.cs
@@ -1,18 +1,40 @@
+using System.Text.Json.Serialization;
 using EnglishLearning.Dictionary.Host.Infrastructure;
+using EnglishLearning.Utilities.General.Extensions;
+using EnglishLearning.Utilities.Identity.Configuration;
+using EnglishLearning.Utilities.Persistence.Redis.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace EnglishLearning.Dictionary.Host
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services
+                .AddControllers(options => options.AddEnglishLearningIdentityFilters())
+                .AddJsonOptions(options =>
+                {
+                    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+                });
 
+            services
+                .AddRedis(Configuration)
+                .AddEnglishLearningIdentity();
+            
             services.AddSwaggerDocumentation();
             
             services.AddCors(options =>
@@ -29,6 +51,8 @@ namespace EnglishLearning.Dictionary.Host
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseEnglishLearningExceptionMiddleware();
+            
             app.UseCors("CorsPolicy");
 
             app.UseSwaggerDocumentation();
