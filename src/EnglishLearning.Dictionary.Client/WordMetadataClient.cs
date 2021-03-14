3120f230b8ace1757f1934a5c3d4ea2dfabc4a10
LytvyniukDima <dima.lytvyniuk@gmail.com>
2021-03-14 20:05:47 +0200

    feat: add http client

---
+++ src/EnglishLearning.Dictionary.Client/WordMetadataClient.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using EnglishLearning.Dictionary.Web.Contracts.Metadata;
+using EnglishLearning.Utilities.Http.Extensions;
+
+namespace EnglishLearning.Dictionary.Client
+{
+    public class WordMetadataClient
+    {
+        private static readonly Uri BaseUrl = new Uri("api/dictionary/word-metadata", UriKind.Relative);
+        private static readonly Uri QueryUrl = new Uri($"{BaseUrl}/query", UriKind.Relative);
+        
+        private readonly HttpClient _httpClient;
+
+        public WordMetadataClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public Task<IReadOnlyList<WordMetadata>> GetWordMetadata(WordMetadataQuery query)
+        {
+            return _httpClient.PostAsync<WordMetadataQuery, IReadOnlyList<WordMetadata>>(QueryUrl, query);
+        }
+    }
+}
